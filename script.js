02f395c073de7c824b712c46c533624c78f5dad5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2015-01-06 15:28:23 +0100

    scripts changed

--- script.js
+++ script.js
@@ -1,80 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
+    var events = new JSEvent();
+    events.register("click:test", function(){alert("test")});
+
     var DOC = document;
     var body = document.querySelector("body");
-
-
-    // [data-form-validator]
+    var formObject = new JSForm();
 
     var validateButton = DOC.querySelector("button");
     validateButton.addEventListener("click", function(event){
-        console.log("clicked");
-        var validation ={
-            integer: function(value) {
-                console.log("integer: " + value);
-            },
-            notempty: function(value) {
-                console.log("notempty: " + value);
-            }
-        };
-
-        var elements = DOC.querySelectorAll("[data-form-validator]");
-        var parsedElements = parse(elements);
-
-        for(var i = 0, length = elements.length; i < length; i++) {
-            var element = elements[i];
-            var validators = element.attributes.getNamedItem("data-form-validator").value.split(",");
-            console.log(validators);
-            for(var j = 0, v_length = validators.length; j < v_length; j++) {
-                var validator = validators[j];
-                validation[validator](parsedElements[element.name]);
-            }
-        }
-
-        console.log(elements);
-        console.log(parsedElements);
+        console.log(formObject.valid(DOC.querySelectorAll("[data-form-validator]")));
     });
 
-    var forms = DOC.querySelectorAll("[data-event-listener][data-event-handler]");
 
-    for(var i = 0, length = forms.length; i < length; i++) {
-        var form = forms[i];
-        form.addEventListener("submit", function(event){
-            console.log("submited");
+    var eventTypes = ["click", "submit"];
+    for(var i = 0, length = eventTypes.length; i < length; i++) {
+        body.addEventListener(eventTypes[i], function(event) {
             var target = event.target;
-            parse(target.elements);
-            event.preventDefault();
+            var registeredEvent = target.attributes.getNamedItem("data-event");
+            if(registeredEvent != undefined) {
+                events.trigger(registeredEvent.value, event, this);
+            }
         });
     }
 
-    function parse(elements) {
-        var data = {};
-
-        for(var i = 0, length = elements.length; i < length; i++) {
-            var element= elements[i];
-
-            if(element.name != undefined && element.name != "") {
-                if(element.type == "radio") {
-                    if(element.checked) {
-                        data[element.name] = element.value;
-                    }
-                } else if(element.type == "checkbox") {
-                    if(element.checked) {
-                        if(data[element.name] == undefined) {
-                            data[element.name] = [];
-                        }
-                        data[element.name].push(element.value);
-                    }
-                } else {
-                    data[element.name] = element.value;
-                }
-            }
-        }
-
-        console.log(data);
-        return data;
-
-    }
+    var forms = DOC.querySelectorAll("[data-event-listener][data-event-handler]");
+    //
+    //for(var i = 0, length = forms.length; i < length; i++) {
+    //    var form = forms[i];
+    //    form.addEventListener("submit", function(event){
+    //        var target = event.target;
+    //        formObject.parse(target.elements);
+    //        event.preventDefault();
+    //    });
+    //}
 
 });
